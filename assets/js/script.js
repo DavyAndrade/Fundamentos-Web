6f4e568902eeee39941cb9b2aa9419df50e297c1
DavyAndrade <hallszindementahhh@gmail.com>
2022-11-17 10:39:17 -0300

    Atualizado com JS

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+
+    // Nome
+    let nome = document.querySelector('#nome');
+    let nomeErro = document.querySelector('#nomeErro');
+
+    // Email
+    let email = document.querySelector('#email');
+    let emailErro = document.querySelector('#emailErro');
+
+    // Assunto
+    let assunto = document.querySelector('#assunto');
+    let assuntoErro = document.querySelector('#assuntoErro');
+
+    // Variáveis Auxiliáveis de Validação
+    let nomeOk = false;
+    let emailOk = false;
+    let assuntoOk = false;
+
+    // Mapa
+    let mapa = document.querySelector('#mapa');
+
+    function validaNome() {
+        if (nome.value.length < 3) {
+            nomeErro.innerHTML = 'Nome Inválido'
+        }else{
+            nomeErro.innerHTML = ''
+            nomeOk = true
+        }
+    }
+
+    function validaEmail(){
+        if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+            emailErro.innerHTML = 'Email Inválido'
+        }else{
+            emailErro.innerHTML = ''
+            emailOk = true
+        }
+    }
+
+    function validaAssunto() {
+        if(assunto.value.length > 100){
+            assuntoErro.innerHTML = 'Limite de Caracteres Excedidos (Limite de 100 Caracteres)'
+        }else{
+            assuntoErro.innerHTML = ''
+            assuntoOk = true
+        }
+    }
+
+    function enviar() {
+        if(nomeOk && emailOk && assuntoOk) {
+            alert('Email enviado com sucesso')
+        }else{
+            alert('Preencha as informações corretas >:[')
+        }
+    }
+
+    function mapaZoom() {
+        mapa.style.width = '600px';
+        mapa.style.height = '400px';
+
+    }
+
+    function mapaNormal() {
+        mapa.style.width = '400px';
+        mapa.style.height = '250px';
+    }
